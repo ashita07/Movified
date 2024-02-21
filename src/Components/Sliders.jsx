3b4da548b694c9761d6a4866ad861f143eca323c
Ashita <ashitam2407@gmail.com>
2024-02-21 17:31:18 +0530

    scrollbar fixed

--- src/Components/Sliders.jsx
+++ src/Components/Sliders.jsx
@@ -14,7 +14,7 @@ getTrendingVideos();
         })
     }
   return (
-    <div className='flex overflow-x-auto px-5 py-4 scrollbar-none'>
+    <div className='flex overflow-x-auto px-5 py-4 scrollbar-hide'>
       {movieList.map((item,index)=>(
          <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md'/>
       ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx,d.ts}",
-];
-export const theme = {
-  // ...
-};
-export const plugins = [
-  require('tailwind-scrollbar-hide')
-  // ...
-];
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
+}
